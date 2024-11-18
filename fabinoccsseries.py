d913f7a319788399a64260048d3ea73fac2541b9
shashank <shashankmuruli@gmail.com>
2024-11-18 19:29:57 +0530

    question answer

---
+++ fabinoccsseries.py
@@ -0,0 +1,7 @@
+number=7
+# num1=0
+# num2=1
+fib=0
+for num in range(number):
+    fib=fib+(num)
+    print(fib)
---
+++ factorial.py
@@ -0,0 +1,5 @@
+number=7
+fact=1
+for num in range (number):
+    fact=fact*(num+1)
+print(fact)
